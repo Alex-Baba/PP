d05b995ebaa49655a28026afdece51858cc18ba4
Alex-Baba <63092587+Alex-Baba@users.noreply.github.com>
2020-05-22 10:39:08 +0300

    Update grupare.kt

--- Lab4/grupare.kt
+++ Lab4/grupare.kt
@@ -1,9 +1,4 @@
-class Comuna(val nume:String):Grupare {
-    val sate:MutableList<Sat> = mutableListOf()
-    fun AddSat(s:Sat)
-    {
-        sate.add(s)
-    }
-    fun getName() = nume
-
+  
+interface Grupare {
+    fun isCapitala() = false
 }
